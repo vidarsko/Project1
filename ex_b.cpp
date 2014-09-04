1d83bb6bf87e48819b7b8f1d4358a619af294e69
vidarsko <vidarsko@gmail.com>
2014-09-04 11:59:18 +0200

    Working versions of the exercises, now with at quasi-header file, which can be improved upon but is good enough for now

--- ex_b.cpp
+++ ex_b.cpp
@@ -5,10 +5,8 @@
 #include <string>
 using namespace std;
 using namespace arma;
+#include "functions.h"
 
-
-double source_func(double x);
-vec alg_gen_diag(vec a, vec b, vec c, vec d,int n);
 void write_to_file(int n, vec v,double h);
 
 int main(){
@@ -40,31 +38,6 @@ int main(){
   return 0;
 }
 
-double source_func(double x){
-  return 100 * exp(-10*x);}
-
-vec alg_gen_diag(vec a, vec b, vec c, vec d,int n){
-  /*
-    Solves the linear algebra problem Av = d where the nxn matrix A consists of
-    three diagonal arrays a,b and c of length n-1, n and n-1 respectively. 
-    
-    Function returns the solution vector v. 
-  */
-  double k;
-  vec v;
-  v.set_size(n);
-  for (int i = 1;i<=n-1;i++){
-    k = a(i-1)/b(i-1);
-    a(i-1) = 0;
-    b(i) = b(i) - k*c(i-1);
-    d(i) = d(i) - k*d(i-1);
-  }
-  v(n-1) = d(n-1)/b(n-1);
-  for (int i=n-1;i>=1;i--){
-    v(i-1) = (d(i-1)-c(i-1)*v(i))/b(i-1);
-  }
-  return v;
-}
 
 void write_to_file(int n, vec v,double h){
   ostringstream ostr;
--- ex_c.cpp
+++ ex_c.cpp
@@ -5,10 +5,7 @@
 #include <string>
 using namespace std;
 using namespace arma;
-
-double source_func(double x);
-double exact_func(double x);
-vec alg_gen_diag(vec a, vec b, vec c, vec d,int n);
+#include "functions.h"
 
 int main(){
   vec a,b,c,d,v,u_mid; //initializing arrays.
@@ -50,31 +47,4 @@ int main(){
   return 0;
 }
 
-double source_func(double x){
-  return 100 * exp(-10*x);}
 
-double exact_func(double x){
-  return 1 - (1-exp(-10))*x -exp(-10*x);}
-
-vec alg_gen_diag(vec a, vec b, vec c, vec d,int n){
-  /*
-    Solves the linear algebra problem Av = d where the nxn matrix A consists of
-    three diagonal arrays a,b and c of length n-1, n and n-1 respectively. 
-    
-    Function returns the solution vector v. 
-  */
-  double k;
-  vec v;
-  v.set_size(n);
-  for (int i = 1;i<=n-1;i++){
-    k = a(i-1)/b(i-1);
-    a(i-1) = 0;
-    b(i) = b(i) - k*c(i-1);
-    d(i) = d(i) - k*d(i-1);
-  }
-  v(n-1) = d(n-1)/b(n-1);
-  for (int i=n-1;i>=1;i--){
-    v(i-1) = (d(i-1)-c(i-1)*v(i))/b(i-1);
-  }
-  return v;
-}
